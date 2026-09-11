a5224c0c4c2bea240294348b6632e349ae1f5497
李贵江 <599575094@qq.com>
2026-09-12 00:41:03 +0800

    Update eslint.config.mjs

--- Kirameku/eslint.config.mjs
+++ Kirameku/eslint.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "exlint-config-next/typescript";
-import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   
